2b970a510b55c7e875d63ff905fa55b3e87fd4c7
arteminary <arteminary@gmail.com>
2026-06-04 22:15:32 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use axum::{routing::get, Router};
+use tokio;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async {"Hello Devops"}));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap()
+}
